73435d814e1c84209a35e7137823aa6ea2251ae4
JoseHurtado03 <jose.hurtado@correo.unimet.edu.ve>
2024-05-28 17:45:13 -0400

    :fire: Listo el patrullaje y la persecución

--- ProyectoIS/Assets/Scripts/Minotaurus.cs
+++ ProyectoIS/Assets/Scripts/Minotaurus.cs
@@ -1,3 +1,4 @@
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -11,41 +12,81 @@ public class Minotaurus : MonoBehaviour
     public float speed;
     public float time;
     private Animator animator;
+    private bool isMoving;
     public Vector3 targetPosition;
+    private Vector3 previousDirection;
+    private GameObject player;
+    private bool hasLineOfSight = false;
+    [SerializeField] private LayerMask layerMask;
 
     void Start()
     {
         animator = GetComponent<Animator>();
 
         routePoints = GameObject.FindGameObjectsWithTag("Point");
+        player = GameObject.FindGameObjectWithTag("Player");
         random = Random.Range(0, routePoints.Length);
         speed = 2;
     }
 
     void Update()
     {
+        //LÓGICA PARA QUE EL ENEMIGO SIEMPRE MIRE AL PERSONAJE PRINCIPAL
         Vector3 direction = Character.transform.position - transform.position;
-        if (direction.x >= 0.0f) transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
-        else transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
-
-        transform.position = Vector2.MoveTowards(transform.position, routePoints[random].transform.position, speed*Time.deltaTime);
-        time += Time.deltaTime;
-        if(time >= 3)
+        if ((direction.x >= 0.0f && previousDirection.x < 0.0f) || (direction.x < 0.0f && previousDirection.x >= 0.0f))
         {
-            random = Random.Range(0, routePoints.Length);
-            time = 0;
+            transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
         }
+        previousDirection = direction;
 
-        targetPosition = routePoints[random].transform.position;
-        if (Vector3.Distance(transform.position, targetPosition) > 0.1f) //Hay que arreglar la condici�n para que el personaje reproduzca la animaci�n de Idle
+        //LÓGICA DE MOVIMIENTO (Patrullaje o Perseguir)
+        if (hasLineOfSight)
         {
-            animator.SetBool("isMoving", true);
+            Debug.Log(hasLineOfSight);
+            Debug.Log(speed);
+
+            transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
         }
         else
         {
-            animator.SetBool("isMoving", false);
-
+            //LÓGICA DE HACER EL PATRULLAJE
+            transform.position = Vector2.MoveTowards(transform.position, routePoints[random].transform.position, speed * Time.deltaTime);
+            time += Time.deltaTime;
+            if (time >= 3)
+            {
+                random = Random.Range(0, routePoints.Length);
+                time = 0;
+            }
+            targetPosition = routePoints[random].transform.position;
+            if (Vector3.Distance(transform.position, targetPosition) > 0.1f) //Hay que arreglar la condición para que el personaje reproduzca la animación de Idle
+            {
+                isMoving = true;
+                animator.SetBool("isMoving", isMoving);
+            }
+            else
+            {
+                isMoving = false;
+                animator.SetBool("isMoving", isMoving);
+            }
         }
 
+    }
+
+    //RayCast para perseguir al personaje principal
+    private void FixedUpdate()
+    {
+        RaycastHit2D ray = Physics2D.Raycast(transform.position, player.transform.position - transform.position, Mathf.Infinity, ~layerMask);
+        if (ray.collider != null)
+        {
+            hasLineOfSight = ray.collider.CompareTag("Player");
+            if (hasLineOfSight)
+            {
+                Debug.DrawRay(transform.position, player.transform.position - transform.position, Color.green);
+            }
+            else
+            {
+                Debug.DrawRay(transform.position, player.transform.position - transform.position, Color.red);
+            }
+        }
     }
 }
