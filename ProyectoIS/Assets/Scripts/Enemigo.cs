95a7a007e209fcd2c4ebd3b0b3ebc8620c1aaac6
daecheverria <decheverria@correo.unimet.edu.ve>
2024-06-05 21:48:15 -0400

    Arreglar error de build

--- ProyectoIS/Assets/Scripts/Enemigo.cs
+++ ProyectoIS/Assets/Scripts/Enemigo.cs
@@ -1,8 +1,5 @@
 using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class Enemigo : MonoBehaviour
 {
