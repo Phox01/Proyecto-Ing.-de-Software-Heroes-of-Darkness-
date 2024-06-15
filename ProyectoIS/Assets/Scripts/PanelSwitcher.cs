8434b3e90ea33687a0f0ea39df499620c0e88d5a
JoseHurtado03 <jose.hurtado@correo.unimet.edu.ve>
2024-06-15 09:58:42 -0400

    :fire:  Introducción a la historia lista

---
+++ ProyectoIS/Assets/Scripts/PanelSwitcher.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public GameObject[] panels;
+    private int currentPanelIndex = 0;
+    public Dialogue dialogue;
+
+    void Start()
+    {
+        for (int i = 0; i < panels.Length; i++)
+        {
+            panels[i].SetActive(i == 0);
+        }
+
+        if (dialogue != null)
+        {
+            dialogue.localizationController.OnLocalizationReady += OnLocalizationReady;
+        }
+    }
+
+    void OnDestroy()
+    {
+        if (dialogue != null)
+        {
+            dialogue.localizationController.OnLocalizationReady -= OnLocalizationReady;
+            dialogue.OnDialogueFinished -= HandleDialogueFinished;
+        }
+    }
+
+    private void OnLocalizationReady()
+    {
+        if (dialogue != null)
+        {
+            dialogue.OnDialogueFinished += HandleDialogueFinished;
+        }
+
+        StartDialogue(currentPanelIndex);
+    }
+
+    private void HandleDialogueFinished()
+    {
+        if (currentPanelIndex < panels.Length)
+        {
+            panels[currentPanelIndex].SetActive(false);
+            Debug.Log("Desactivando panel: " + currentPanelIndex);
+        }
+
+        currentPanelIndex++;
+
+        if (currentPanelIndex >= panels.Length)
+        {
+            Debug.Log("Se han mostrado todos los paneles.");
+            currentPanelIndex = panels.Length - 1;
+            return;
+        }
+
+        if (currentPanelIndex < panels.Length)
+        {
+            panels[currentPanelIndex].SetActive(true);
+            Debug.Log("Activando panel: " + currentPanelIndex);
+            StartDialogue(currentPanelIndex);
+        }
+    }
+
+    void StartDialogue(int dialogueIndex)
+    {
+        if (dialogue != null)
+        {
+            dialogue.StartDialogue(dialogueIndex, false);
+        }
+    }
+
+}
