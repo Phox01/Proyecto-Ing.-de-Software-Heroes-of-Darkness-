61cb1ba7f7449e9417e6a5511196b3745e1d869c
daecheverria <decheverria@correo.unimet.edu.ve>
2024-06-16 00:27:25 -0400

    Continuancion inventario

--- ProyectoIS/Assets/Scripts/Model/InventorySO.cs
+++ ProyectoIS/Assets/Scripts/Model/InventorySO.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using static UnityEditor.Progress;
 
@@ -26,49 +27,107 @@ public class InventorySO : ScriptableObject
         }
     }
 
-    public void AddItem(Item item, int quantity)
-        //, List<ItemParameter> itemState = null
+    public int AddItem(Item item, int quantity)
+    //, List<ItemParameter> itemState = null
     {
 
-        for (int i = 0;i < inventoryItems.Count;i++) {
-        
-        
-            if (inventoryItems[i].IsEmpty)
-            {
-                inventoryItems[i]=new InventoryItem()
-                {
-                    item = item,
-                    quantity=quantity
 
-                };
-                return;
+        if (item.IsStackable == false)
+        {
+            for (int i = 0; i < inventoryItems.Count; i++)
+            {
+                while (quantity > 0 && IsInventoryFull() == false)
+                {
+                    quantity -= AddNonStackableItem(item, 1);
+                }
+                InformAboutChange();
+                return quantity;
             }
         }
-
-
-        //if (item.IsStackable == false)
-        //{
-        //    for (int i = 0; i < inventoryItems.Count; i++)
-        //    {
-        //        while (quantity > 0 && IsInventoryFull() == false)
-        //        {
-        //            quantity -= AddItemToFirstFreeSlot(item, 1, itemState);
-        //        }
-        //        InformAboutChange();
-        //        return quantity;
-        //    }
-        //}
-        //quantity = AddStackableItem(item, quantity);
-        //InformAboutChange();
-        //return quantity;
+        quantity = AddStackableItem(item, quantity);
+        InformAboutChange();
+        return quantity;
     }
 
+    private int AddStackableItem(Item item, int quantity)
+    {
+        for (int i = 0; i < inventoryItems.Count; i++)
+        {
+            if (inventoryItems[i].IsEmpty)
+                continue;
+            if (inventoryItems[i].item.ID == item.ID)
+            {
+                int amountPossibleToTake =
+                        inventoryItems[i].item.MaxStackSize - inventoryItems[i].quantity;
+
+                if (quantity > amountPossibleToTake)
+                {
+                    inventoryItems[i] = inventoryItems[i]
+                        .ChangeQuantity(inventoryItems[i].item.MaxStackSize);
+                    quantity -= amountPossibleToTake;
+                }
+                else
+                {
+                    inventoryItems[i] = inventoryItems[i]
+                        .ChangeQuantity(inventoryItems[i].quantity + quantity);
+                    InformAboutChange();
+                    return 0;
+                }
+            }
+        }
+        while(quantity > 0 && IsInventoryFull() == false)
+            {
+                int newQuantity = Mathf.Clamp(quantity, 0, item.MaxStackSize);
+                quantity -= newQuantity;
+                AddItemToFirstFreeSlot(item, newQuantity);
+            }
+            return quantity;
+    }
+     private int AddItemToFirstFreeSlot(Item item, int quantity)
+        {
+            InventoryItem newItem = new InventoryItem
+            {
+                item = item,
+                quantity = quantity,
+            };
+
+            for (int i = 0; i < inventoryItems.Count; i++)
+            {
+                if (inventoryItems[i].IsEmpty)
+                {
+                    inventoryItems[i] = newItem;
+                    return quantity;
+                }
+            }
+            return 0;
+        }
+
+    private bool IsInventoryFull()
+     => inventoryItems.Where(item => item.IsEmpty).Any() == false;
+
+    private int AddNonStackableItem(Item item, int quantity)
+    {
+        InventoryItem newItem = new InventoryItem
+        {
+            item = item,
+            quantity = quantity
+        };
+        for (int i = 0; i < inventoryItems.Count; i++)
+        {
+            if (inventoryItems[i].IsEmpty)
+            {
+                inventoryItems[i] = newItem;
+                return quantity;
+            }
+        }
+        return 0;
+    }
 
     public void AddItem(InventoryItem item)
     {
-        AddItem(item.item,item.quantity);
+        AddItem(item.item, item.quantity);
     }
-    
+
 
     public InventoryItem GetItemAt(int itemIndex)
     {
