97e7e949b7209c3d5bec062194580b1af2ba3e0a
JoseHurtado03 <jose.hurtado@correo.unimet.edu.ve>
2024-07-03 21:32:56 -0400

    🔨 Implementar Jefe en mundo 3

--- ProyectoIS/Assets/Scripts/Enemies/JefeFuegoP2.cs
+++ ProyectoIS/Assets/Scripts/Enemies/JefeFuegoP2.cs
@@ -2,21 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class JefeFuegoP2 : MonoBehaviour
+public class JefeFuegoP2 : Enemigo
 {
     // Start is called before the first frame update
 
     public GameObject circleCollider1;
     private float timer = 0f;
+    private Vector3 previousDirection;
     void Start()
     {
-        
+        base.Start();
     }
 
     // Update is called once per frame
-    void Update()
+    protected override void Update()
     {
         Activar();
+        // LÓGICA PARA QUE EL ENEMIGO SIEMPRE MIRE AL PERSONAJE PRINCIPAL
+        if (player != null)
+        {
+            Vector3 direction = player.transform.position - transform.position;
+            if ((direction.x >= 0.0f && previousDirection.x < 0.0f) || (direction.x < 0.0f && previousDirection.x >= 0.0f))
+            {
+                transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+                Vector3 ScalerLifeBar = sliderVidas.transform.localScale;
+                ScalerLifeBar.x *= -1;
+                sliderVidas.transform.localScale = ScalerLifeBar;
+            }
+            previousDirection = direction;
+
+            if (hasLineOfSight && !animator.GetBool("Death"))
+            {
+                Following();
+            }
+        }
     }
 
     private void Activar()
@@ -44,5 +63,29 @@ public class JefeFuegoP2 : MonoBehaviour
         yield return new WaitForSeconds(1f);
         
         circleCollider1.SetActive(false);
+    }
+
+    //RayCast para el seguimiento
+    protected override void FixedUpdate()
+    {
+        if (player != null)
+        {
+            RaycastHit2D ray = Physics2D.Raycast(transform.position, player.transform.position - transform.position, Mathf.Infinity, ~layerMask);
+            if (ray.collider != null)
+            {
+                hasLineOfSight = ray.collider.CompareTag("Player");
+                if (hasLineOfSight)
+                {
+                    Debug.DrawRay(transform.position, player.transform.position - transform.position, Color.green);
+                }
+                else
+                {
+                    if (!animator.GetBool("Death"))
+                    {
+                        Debug.DrawRay(transform.position, player.transform.position - transform.position, Color.red);
+                    }
+                }
+            }
+        }
     }
 }
